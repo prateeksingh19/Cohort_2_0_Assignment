a6b518a2f50c3d107a1cb82f7b36e3a9a09e9a4a
Prateek Singh <prateekvns99@gmail.com>
2024-01-07 15:32:10 +0530

    Completed Week 3 Mongo without JWT Auth Assignment

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,18 +1,30 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://admin:Xbc5E68j83YDfy*q@prateek.cffizos.mongodb.net/course_website?authSource=admin&replicaSet=atlas-10nn4q-shard-0&readPreference=primary&ssl=true');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
+    username : String,
+    password : String
     // Schema definition here
 });
 
 const UserSchema = new mongoose.Schema({
+    username : String,
+    password : String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
     // Schema definition here
 });
 
 const CourseSchema = new mongoose.Schema({
+    title : String,
+    description : String,
+    imageLink: String,
+    price : Number
     // Schema definition here
 });
 
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,57 @@
-const { Router } = require("express");
+const express = require("express");
 const adminMiddleware = require("../middleware/admin");
-const router = Router();
+const { Admin, Course } = require("../db");
+const router = express.Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username
+    const password = req.body.password
+
+    const existingAdmin = await Admin.findOne({ username: username });
+
+    if (existingAdmin) {
+        return res.status(400).json({ "msg": "Admin already exists" })
+    }
+    const admin = new Admin({ username: username, password: password });
+    admin.save();
+    res.status(200).json({ "msg": "Admin created successfully" })
+
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+    const username = req.header.username
+    const password = req.header.password
+    const _id = req.params.id
+    const title = req.body.title
+    const description = req.body.description
+    const price = req.body.price
+    const imageLink = req.body.imageLink
+
+    const existingCourse = await Course.findOne({ title: title });
+    if (existingCourse) {
+        return res.status(400).json({ "msg": "Course already exists" })
+    }
+
+    const course = new Course({ title: title, description: description, price: price, imageLink: imageLink });
+    course.save();
+    res.status(200).json({
+        message: 'Course created successfully', courseId: course._id
+    })
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async (req, res) => {
     // Implement fetching all courses logic
+    const username = req.header.username
+    const password = req.header.password
+
+    const response = await Course.find({});
+
+    res.json({
+        courses: response
+    })
 });
 
 module.exports = router;
