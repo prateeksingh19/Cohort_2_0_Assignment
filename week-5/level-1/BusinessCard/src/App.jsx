b823bd2fefa7b11f88929741cdaad4e9ab48a9a0
Prateek Singh <prateekvns99@gmail.com>
2024-02-19 14:48:09 +0530

    Completed FrontEnd

--- week-5/level-1/BusinessCard/src/App.jsx
+++ week-5/level-1/BusinessCard/src/App.jsx
@@ -1,8 +1,20 @@
 import { useState } from "react";
 import "./App.css";
+import BusinessCard from "./Components/BusinessCard";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BusinessCard
+        title="Title"
+        description="description"
+        interests={["Cricket", "Football"]}
+        linkedIn="https://www.linkedin.com/feed/"
+        twitter="https://twitter.com/home"
+        otherLinks="https://github.com/"
+      />
+    </>
+  );
 }
 
 export default App;
---
+++ week-5/level-1/BusinessCard/src/Components/BusinessCard.jsx
@@ -0,0 +1,24 @@
+export default function BusinessCard(props) {
+  const interest = props.interests;
+  const interestElements = interest.map((interests, index) => (
+    <li key={index}>{interests}</li>
+  ));
+
+  return (
+    <div>
+      <p className="title">{props.title}</p>
+      <p className="description">{props.description}</p>
+      <p className="interests">Interests</p>
+      <ul>{interestElements}</ul>
+      <a href={props.linkedIn} target="_blank" rel="noopener noreferrer">
+        <button>LinkedIn</button>
+      </a>
+      <a href={props.twitter} target="_blank" rel="noopener noreferrer">
+        <button>Twitter</button>
+      </a>
+      <a href={props.otherLinks} target="_blank" rel="noopener noreferrer">
+        <button>Other</button>
+      </a>
+    </div>
+  );
+}
